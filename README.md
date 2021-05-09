42b8867c545cef6928918d04b069456ed7a5781e
Tongyu Lu <lutongyu2004@outlook.com>
2021-05-10 01:21:43 +0800

    First bunch of banners

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# site-banner-museum
-Site banners, really.
+# The Website Banner Museum
+
+I thought this would be fun, so, here it is.
+
+[Suggest a banner / file an issue](/StevenLu2004/site-banner-museum/issues)
---
+++ script.js
@@ -0,0 +1,39 @@
+
+let applyRandomColor = function(el, toned=false, light=null) {
+	let hue = Math.floor(Math.random() * 360);
+	let saturation = Math.floor(Math.random() * 101);
+	let lightness = Math.floor(Math.random() * 101);
+	if (toned) {
+		lightness = Math.round(lightness * .15);
+		if (light === true || (light === null && Math.random() < .5)) {
+			lightness += 85;
+		} else {
+			lightness += 10;
+		}
+	}
+	el.style.backgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`;
+	el.style.color = (lightness > 60) ? 'black' : 'white';
+};
+
+let applyAllRandomColors = function() {
+	// Untoned
+	for (let el of document.getElementsByClassName('random-color')) {
+		applyRandomColor(el, toned=false);
+	}
+	// Toned
+	for (let el of document.getElementsByClassName('random-color-toned')) {
+		applyRandomColor(el, toned=true);
+	}
+	// Dark
+	for (let el of document.getElementsByClassName('random-color-dark')) {
+		applyRandomColor(el, toned=true, light=false);
+	}
+	// Light
+	for (let el of document.getElementsByClassName('random-color-light')) {
+		applyRandomColor(el, toned=true, light=true);
+	}
+};
+
+window.addEventListener('load', () => {
+	applyAllRandomColors();
+});
